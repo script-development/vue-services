483f0eb8465c7d088406b1fd2ef5afe83f27a740
Gerard Oosterhof <gerard@scripthelpt.nl>
2020-07-10 11:56:40 +0200

    Type updates

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,13 @@
+export { RouterService } from '../src/services/router';
+export { StoreService } from '../src/services/store';
+export { ErrorService } from '../src/services/error';
+export { LoadingService } from '../src/services/loading';
+export { AuthService } from '../src/services/auth';
+export { HTTPService } from '../src/services/http';
+export { EventService } from '../src/services/event';
+export { PageCreatorService } from '../src/services/pageCreator';
+export {TranslatorService} from '../src/services/translator'
+
 export {
     routerService,
     storeService,
