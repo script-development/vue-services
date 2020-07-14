4532ce8b9829ff51adeb02d440acda797d3f34ab
Gerard Oosterhof <gerard@scripthelpt.nl>
2020-07-14 09:40:19 +0200

    Updated types again

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,43 +1,25 @@
-export const routerService: import('../src/services/router').RouterService;
-export const storeService: import('../src/services/store').StoreService;
-export const errorService: import('../src/services/error').ErrorService;
-export const loadingService: import('../src/services/loading').LoadingService;
-export const authService: import('../src/services/auth').AuthService;
-export const httpService: import('../src/services/http').HTTPService;
-export const eventService: import('../src/services/event').EventService;
-export const pageCreatorService: import('../src/services/pageCreator').PageCreatorService;
-export const translatorService: import('../src/services/translator').TranslatorService;
 export {BaseController} from './controllers';
 
-// TODO :: move to errors
-// export class MissingTranslationError extends Error {
-//     constructor(...params: any[]);
-// }
+import {
+    HTTPService,
+    EventService,
+    TranslatorService,
+    AuthService,
+    ErrorService,
+    LoadingService,
+    PageCreatorService,
+} from './services';
 
-// export class MissingDefaultLoggedinPageError extends Error {
-//     constructor(...params: any[]);
-// }
+import {RouterService} from './routerService';
 
-// import {
-//     HTTPService,
-//     EventService,
-//     TranslatorService,
-//     AuthService,
-//     ErrorService,
-//     LoadingService,
-//     PageCreatorService,
-// } from './services';
+import {StoreService} from './storeService';
 
-// import {RouterService} from './routerService';
-
-// import {StoreService} from './storeService';
-
-// export const authService: AuthService;
-// export const routerService: RouterService;
-// export const storeService: StoreService;
-// export const errorService: ErrorService;
-// export const loadingService: LoadingService;
-// export const httpService: HTTPService;
-// export const eventService: EventService;
-// export const pageCreatorService: PageCreatorService;
-// export const translatorService: TranslatorService;
+export const authService: AuthService;
+export const routerService: RouterService;
+export const storeService: StoreService;
+export const errorService: ErrorService;
+export const loadingService: LoadingService;
+export const httpService: HTTPService;
+export const eventService: EventService;
+export const pageCreatorService: PageCreatorService;
+export const translatorService: TranslatorService;
