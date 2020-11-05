fbf28e66e2d8850990073b9ac51c6c2d16ef1ec3
Gerard Oosterhof <gerard@script.nl>
2020-11-05 12:58:47 +0100

    Moved test fixtures to a setup script in the test folder
    Problem
    Stryker could not read the fixtures
    Solution
    Moved the fixtures so they are included in the test
    Note

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,5 @@
 // since the test is running in node, localStorage is not defined, so we mock it
-const localStorageMock = {
+global.localStorage = {
     data: {},
 
     getItem(key) {
@@ -15,8 +15,6 @@ const localStorageMock = {
         this.data = {};
     },
 };
-
-global.localStorage = localStorageMock;
 
 // TODO :: should be a better option then this
 // http service instantiates an axios instance and it's unaccessable from outside
