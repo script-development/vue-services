0aea86d784cf02d2f090d5630df8c48ea932f932
Gerard Oosterhof <gerard@script.nl>
2020-11-11 10:20:47 +0100

    Loading test time increased
    Problem
    Loading test was taking to long
    Solution
    Exposed the timeouts to be set
    Note

--- test/src/services/loading.js
+++ test/src/services/loading.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 
 const {strictEqual} = assert;
 
-import {loading, setLoading} from '../../../src/services/loading';
+import {loading, setLoading, setMinTimeSpinner, setSpinnerTimeout} from '../../../src/services/loading';
 
 const sleep = time => {
     return new Promise(resolve => {
@@ -13,42 +13,52 @@ const sleep = time => {
 };
 
 describe('Loading Service', () => {
-    describe('test stored loading value', () => {
-        it('initial value of loading should be false', () => {
+    describe('initial value', () => {
+        it('should be false', () => {
             strictEqual(loading.value, false);
         });
     });
-    describe('test setting new loading value', () => {
-        it('should be false when loading is already false', async () => {
+
+    describe('setLoading function', () => {
+        // setting spinner timeout and mintime spinner to lower value, so the tests speed up
+        setSpinnerTimeout(20);
+        setMinTimeSpinner(20);
+
+        it('should not change loading when loading is already false', async () => {
             setLoading(false);
             strictEqual(loading.value, false);
         });
-        it('should be false immediatly after calling the function', () => {
+
+        it('should not immediatly set loading to true after calling the function with loading = true', () => {
             setLoading(true);
             strictEqual(loading.value, false);
         });
-        it('should be true after 500ms', async () => {
+
+        it('should set loading to true after spinnerTimeout time', async () => {
             setLoading(true);
-            await sleep(500);
+            await sleep(20);
             strictEqual(loading.value, true);
         });
-        it('should be true after 1000ms', async () => {
+
+        it('should not set loading to false after calling setloading with false when calling the function when the mintimespinner is active', async () => {
             setLoading(true);
-            await sleep(500);
+            await sleep(25);
             setLoading(false);
-            await sleep(500);
+            await sleep(10);
             strictEqual(loading.value, true);
         });
-        it('should be false after 1500ms', async () => {
+
+        it('should set loading to false after the mintimespinner time has been done', async () => {
             setLoading(true);
-            await sleep(500);
+            await sleep(25);
             setLoading(false);
-            await sleep(1000);
+            await sleep(25);
             strictEqual(loading.value, false);
         });
-        it('should be false after 1000ms', async () => {
+
+        it('should set loading to false after calling the function with false after the mintimespinner time has elapsed', async () => {
             setLoading(true);
-            await sleep(1900);
+            await sleep(50);
             setLoading(false);
             strictEqual(loading.value, true);
         });
