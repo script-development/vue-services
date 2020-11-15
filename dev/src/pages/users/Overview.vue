9e9e4ee8ef22b2963d2b519f16a447a6cbd62834
Gerard Oosterhof <gerard.oosterhof@gmail.com>
2020-11-15 14:03:54 +0100

    Working example!

--- dev/src/pages/users/Overview.vue
+++ dev/src/pages/users/Overview.vue
@@ -1,7 +1,26 @@
 <template>
     <div>OVERVIEW</div>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ users }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
-export default {};
+import {computed} from 'vue';
+import {usersModule} from '../../controllers/users';
+export default {
+    setup() {
+        const users = usersModule.getAll;
+        return {users};
+    },
+};
 </script>
